9460f29ad2540342b656a9d174e7e8c56e1ae6d6
Plew Woo <pew_woothiphom@hotmail.com>
2022-08-02 16:43:07 +0700

    edit pagination

--- app.js
+++ app.js
@@ -61,14 +61,22 @@ const exphbs = hbs.create({
         paginationIndex: (value, pageIndex) => {
             return (value + 1) + (10 * pageIndex)
         },
-        ifPagination: (value1, value2) => {
+        usersPagination: (value1, value2) => {
             if (value1 == value2) {
                 return `<li class="page-item active"><a class="page-link" href="/users/${value2}">${value2}</a></li>`
             }
             else {
                 return `<li class="page-item"><a class="page-link" href="/users/${value2}">${value2}</a></li>`
             }
-        }
+        },
+        historiesPagination: (value1, value2) => {
+            if (value1 == value2) {
+                return `<li class="page-item active"><a class="page-link" href="/histories/${value2}">${value2}</a></li>`
+            }
+            else {
+                return `<li class="page-item"><a class="page-link" href="/histories/${value2}">${value2}</a></li>`
+            }
+        },
     }
 })
 
