13f4316b08a4ba861f07fb98a2935717f15d9e12
Plew Woo <pew_woothiphom@hotmail.com>
2022-08-12 11:08:05 +0700

    edit database

--- database/database.js
+++ database/database.js
@@ -5,26 +5,13 @@ let mongoDbTest = process.env.mongoDbTest
 let mongoDbProd = process.env.mongoDbProd
 var db = mongoose.connection;
 
-if (process.env.isHeroku == true){
-    mongoose.connect(process.env.MONGO_DATABASE, { useNewUrlParser: true });
-    db.on('connected', () => {
-        console.log('Database is connected successfully');
-    });
-    db.on('disconnected', () => {
-        console.log('Database is disconnected successfully');
-    })
-    db.on('error', console.error.bind(console, 'connection error:'));
-}
-else {
-    mongoose.connect(mongoDbProd, { useNewUrlParser: true });
-    db.on('connected', () => {
-        console.log('Database is connected successfully');
-    });
-    db.on('disconnected', () => {
-        console.log('Database is disconnected successfully');
-    })
-    db.on('error', console.error.bind(console, 'connection error:'));
-}
-
+mongoose.connect(process.env.MONGO_DATABASE, { useNewUrlParser: true });
+db.on('connected', () => {
+    console.log('Heroku Database is connected successfully');
+});
+db.on('disconnected', () => {
+    console.log('Heroku Database is disconnected successfully');
+})
+db.on('error', console.error.bind(console, 'connection error:'));
 
 module.exports = db;
