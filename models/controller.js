1f7e553d76e65f810bdfb44d3b490723cc719923
Plew Woo <pew_woothiphom@hotmail.com>
2022-08-12 10:24:30 +0700

    add Promise async&await

--- models/controller.js
+++ models/controller.js
@@ -5,164 +5,157 @@ let histories = 0
 
 const db = {
 	usersCount: async (cb) => {
-		conn.collection('users').count({}, (err, result) => {
-			if (err)
-				cb(err, null);
-			cb(null, result);
-
-			users = result;
-			return users;
+		return new Promise((resolve, reject) => {
+			conn.collection('users').count({}, (err, result) => {
+				resolve(result)
+	
+				users = result;
+				return users;
+			})
 		})
 	},
 	historiesCount: async (cb) => {
-		conn.collection('histories').count({}, (err, result) => {
-			if (err)
-				cb(err, null)
-			cb(null, result)
-
-			histories = result
-			return histories
+		return new Promise((resolve, reject) => {
+			conn.collection('histories').count({}, (err, result) => {
+				resolve(result)
+	
+				histories = result
+				return histories
+			})
 		})
 	},
 	selectAllUsers: async (page, resultsPerPage, cb) => {
-		conn.collection('users').find({}).limit(resultsPerPage).skip(page > 0 ? page * resultsPerPage : 0).sort({ 'createdAt': -1 }).toArray((err, result) => {
-			if (err)
-				cb(err, null);
-			cb(null, {
-				'totalUsers': users,
-				'page': page,
-				'pagination': page + 1,
-				'users': result
+		return new Promise((resolve, reject) => {
+			conn.collection('users').find({}).limit(resultsPerPage).skip(page > 0 ? page * resultsPerPage : 0).sort({ 'createdAt': -1 }).toArray((err, result) => {
+				resolve({
+					'totalUsers': users,
+					'page': page,
+					'pagination': page + 1,
+					'users': result
+				})
 			});
-		});
-	},
-	selectUser: (id, cb) => {
-		conn.collection('users').findOne({ _id: id }, (err, result) => {
-			if (err)
-				cb(err, null);
-			cb(null, result);
-		});
+		})
 	},
 	select10UsersAndHistories: async (cb) => {
-		conn.collection('histories').aggregate([
-			{
-				$lookup:
+		return new Promise((resolve, reject) => {
+			conn.collection('histories').aggregate([
 				{
-					from: 'users',
-					localField: 'userId',
-					foreignField: '_id',
-					as: 'user'
-				}
-			},
-			{
-				$match: { 'users': { $ne: [] } }
-			},
-		]).sort({ 'createdAt': -1 }).limit(10).toArray((err, result) => {
-			if (err)
-				cb(err, null);
-			cb(null, result);
-		});
-	},
-	selectAllUsersAndHistories: (page, resultsPerPage, cb) => {
-		conn.collection('histories').aggregate([
-			{
-				$lookup:
+					$lookup:
+					{
+						from: 'users',
+						localField: 'userId',
+						foreignField: '_id',
+						as: 'user'
+					}
+				},
 				{
-					from: 'users',
-					localField: 'userId',
-					foreignField: '_id',
-					as: 'user'
-				}
-			},
-			{
-				$match: { 'users': { $ne: [] } }
-			},
-			{
-				$sort: { 'createdAt': -1 }
-			},
-			{
-				$skip: page > 0 ? page * resultsPerPage : 0
-			},
-			{
-				$limit: resultsPerPage
-			},
-		]).toArray(function (err, result) {
-			if (err)
-				cb(err, null);
-			cb(null, {
-				'totalHistories': histories,
-				'page': page,
-				'pagination': page + 1,
-				'histories': result
+					$match: { 'users': { $ne: [] } }
+				},
+			]).sort({ 'createdAt': -1 }).limit(10).toArray((err, result) => {
+				resolve(result)
 			});
-		});
+		})
 	},
-	selectUserAndHistory: (id, cb) => {
-		conn.collection('users').aggregate([
-			{
-				$match: { '_id': id }
-			},
-			{
-				$lookup:
+	selectAllUsersAndHistories: async (page, resultsPerPage, cb) => {
+		return new Promise((resolve, reject) => {
+			conn.collection('histories').aggregate([
 				{
-					from: 'histories',
-					localField: '_id',
-					foreignField: 'userId',
-					as: 'history'
-				}
-			},
-			{
-				$lookup:
+					$lookup:
+					{
+						from: 'users',
+						localField: 'userId',
+						foreignField: '_id',
+						as: 'user'
+					}
+				},
 				{
-					from: 'badges',
-					localField: 'badge',
-					foreignField: '_id',
-					as: 'badge'
-				}
-			},
-		]).toArray((err, result) => {
-			if (err)
-				cb(err, null);
-			cb(null, result);
-		});
+					$match: { 'users': { $ne: [] } }
+				},
+				{
+					$sort: { 'createdAt': -1 }
+				},
+				{
+					$skip: page > 0 ? page * resultsPerPage : 0
+				},
+				{
+					$limit: resultsPerPage
+				},
+			]).toArray(function (err, result) {
+				resolve({
+					'totalHistories': histories,
+					'page': page,
+					'pagination': page + 1,
+					'histories': result
+				})
+			});	
+		})
 	},
-	selectHistoryAndUser: (id, cb) => {
-		conn.collection('histories').aggregate([
-			{
-				$match: { '_id': id }
-			},
-			{
-				$lookup:
+	selectUserAndHistory: async (id, cb) => {
+		return new Promise((resolve, reject) => {
+			conn.collection('users').aggregate([
 				{
-					from: 'users',
-					localField: 'userId',
-					foreignField: '_id',
-					as: 'user'
-				}
-			},
-			{
-				$lookup:
+					$match: { '_id': id }
+				},
 				{
-					from: 'badges',
-					localField: 'badge',
-					foreignField: '_id',
-					as: 'badge'
-				}
-			},
-		]).toArray((err, result) => {
-			if (err)
-				cb(err, null);
-			cb(null, result);
-		});
+					$lookup:
+					{
+						from: 'histories',
+						localField: '_id',
+						foreignField: 'userId',
+						as: 'history'
+					}
+				},
+				{
+					$lookup:
+					{
+						from: 'badges',
+						localField: 'badge',
+						foreignField: '_id',
+						as: 'badge'
+					}
+				},
+			]).toArray((err, result) => {
+				resolve(result)
+			});
+		})
 	},
-	allUsers: (cb) => {
+	selectHistoryAndUser: async (id, cb) => {
+		return new Promise((resolve, reject) => {
+			conn.collection('histories').aggregate([
+				{
+					$match: { '_id': id }
+				},
+				{
+					$lookup:
+					{
+						from: 'users',
+						localField: 'userId',
+						foreignField: '_id',
+						as: 'user'
+					}
+				},
+				{
+					$lookup:
+					{
+						from: 'badges',
+						localField: 'badge',
+						foreignField: '_id',
+						as: 'badge'
+					}
+				},
+			]).toArray((err, result) => {
+				resolve(result)
+			});
+		})
+	},
+	allUsers: async (cb) => {
 		return new Promise((resolve, reject) => {
 			conn.collection('users').find({}).toArray(function (err, result) {
 				resolve(result)
 			})
 		})
 	},
-	allHistories: (cb) => {
+	allHistories: async (cb) => {
 		return new Promise((resolve, reject) => {
 			conn.collection('histories').find({}).toArray(function (err, result) {
 				resolve(result)
--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,13 @@ const converter = require('json-2-csv');
 var ObjectId = require('mongodb').ObjectId;
 
 router.get('/', async (req, res) => {
-	var userCount
-	var historiesCount
-	var labelsData = [];
-	var chartData = []
+	let userCount
+	let historiesCount
+	let labelsData = [];
+	let chartData = []
 
-	db.usersCount(async (err, result) => {
-		userCount = result
-		return await userCount
-	})
-	db.historiesCount(async (err, result) => {
-		historiesCount = result
-		return await historiesCount
-	})
+	userCount = await db.usersCount()
+	historiesCount = await db.historiesCount()
 
 	const allData = db.allHistories()
 	let hisResult = await allData
@@ -29,7 +23,7 @@ router.get('/', async (req, res) => {
 		monthArr.push(month[1])
 	}
 
-	var labelsData = monthArr.filter(function (item, pos) {
+	labelsData = monthArr.filter(function (item, pos) {
 		return monthArr.indexOf(item) == pos;
 	});
 
@@ -42,18 +36,18 @@ router.get('/', async (req, res) => {
 
 	chartData = Object.values(count)
 
-	db.select10UsersAndHistories((err, result) => {
-		context = {
-			result,
-			userCount,
-			historiesCount,
-			index: true,
-			labelsData,
-			chartData,
-		}
+	const result = await db.select10UsersAndHistories()
 
-		res.render('index', context)
-	})
+	context = {
+		result,
+		userCount,
+		historiesCount,
+		index: true,
+		labelsData,
+		chartData,
+	}
+
+	res.render('index', context)
 });
 
 router.get('/users/:page', async (req, res) => {
@@ -63,72 +57,71 @@ router.get('/users/:page', async (req, res) => {
 	page = page - 1
 
 	db.usersCount((err, result) => { })
+	const result = await db.selectAllUsers(page, resultsPerPage)
 
-	await db.selectAllUsers(page, resultsPerPage, (err, result) => {
-		for (i = 0; i < Math.ceil(result.totalUsers / resultsPerPage); i++) {
-			pageList.push(i + 1)
-		}
+	for (i = 0; i < Math.ceil(result.totalUsers / resultsPerPage); i++) {
+		pageList.push(i + 1)
+	}
 
-		res.render('user/allUser', {
-			result,
-			users: true,
-			pageList,
-		})
+	res.render('user/allUser', {
+		result,
+		users: true,
+		pageList,
 	})
 });
 
-router.get('/user/:id', (req, res) => {
+router.get('/user/:id', async (req, res) => {
 	const id = req.params.id
 	const oid = new ObjectId(id)
 
-	db.selectUserAndHistory(oid, (err, result) => {
-		let labelsData = []
-		let scoreData = []
-		let resData = []
+	const result = await db.selectUserAndHistory(oid)
 
-		for (i = 0; i < result[0].history.length; i++) {
-			let dateArray = result[0].history[i].createdAt
-			labelsData.push(dateArray)
-		}
+	let labelsData = []
+	let scoreData = []
+	let resData = []
 
-		for (i = 0; i < result[0].history.length; i++) {
-			let scoreArray = result[0].history[i].totalScore
-			scoreData.push(scoreArray)
-		}
+	for (i = 0; i < result[0].history.length; i++) {
+		let dateArray = result[0].history[i].createdAt
+		labelsData.push(dateArray)
+	}
 
-		for (i = 0; i < result[0].history.length; i++) {
-			for (j=0; j < result[0].history[i].sections.length; j++) {
-				if (result[0].history.length == 1) {
-					let resArray = result[0].history[0].sections[j].avgReactionTimeMs
-					resData.push(resArray/1000)
-				}
-				else {
-					let resArray = result[0].history[i].sections[j].avgReactionTimeMs
-					resData.push(parseFloat(resArray/1000))
-				}
+	for (i = 0; i < result[0].history.length; i++) {
+		let scoreArray = result[0].history[i].totalScore
+		scoreData.push(scoreArray)
+	}
+
+	for (i = 0; i < result[0].history.length; i++) {
+		for (j = 0; j < result[0].history[i].sections.length; j++) {
+			if (result[0].history.length == 1) {
+				let resArray = result[0].history[0].sections[j].avgReactionTimeMs
+				resData.push(resArray / 1000)
+			}
+			else {
+				let resArray = result[0].history[i].sections[j].avgReactionTimeMs
+				resData.push(parseFloat(resArray / 1000))
 			}
 		}
+	}
 
-		let reactionTimeData = [];
-		let temp = [];
-		
-		for(let i=0; i<result[0].history.length; i++){
-			for(let j=0; j<3; j++){
-				temp.push(resData[0]);
-				resData.shift();
-			}
-			let sum = temp.reduce((a, b) => a + b, 0);
-			temp = [];
-			reactionTimeData.push((sum/3).toFixed(2));
+	let reactionTimeData = [];
+	let temp = [];
+
+	for (let i = 0; i < result[0].history.length; i++) {
+		for (let j = 0; j < 3; j++) {
+			temp.push(resData[0]);
+			resData.shift();
 		}
+		let sum = temp.reduce((a, b) => a + b, 0);
+		temp = [];
+		reactionTimeData.push((sum / 3).toFixed(2));
+	}
 
-		res.render('user/user', {
-			result,
-			users: true,
-			labelsData,
-			scoreData,
-			reactionTimeData
-		})
+	res.render('user/user', {
+		result,
+		users: true,
+		labelsData,
+		scoreData,
+		reactionTimeData
 	})
 });
 
@@ -140,54 +133,65 @@ router.get('/histories/:page', async (req, res) => {
 	page = page - 1
 
 	db.historiesCount((err, result) => { })
+	const result = await db.selectAllUsersAndHistories(page, resultsPerPage)
 
-	await db.selectAllUsersAndHistories(page, resultsPerPage, (err, result) => {
-		for (i = 0; i < Math.ceil(result.totalHistories / resultsPerPage); i++) {
-			pageList.push(i + 1)
-		}
+	for (i = 0; i < Math.ceil(result.totalHistories / resultsPerPage); i++) {
+		pageList.push(i + 1)
+	}
 
-		res.render('history/allHistory', {
-			result,
-			history: true,
-			pageList,
-		})
+	res.render('history/allHistory', {
+		result,
+		history: true,
+		pageList,
 	})
 });
 
-router.get('/history/:id', (req, res) => {
+router.get('/history/:id', async (req, res) => {
 	const id = req.params.id
 	const oid = new ObjectId(id)
 
-	db.selectHistoryAndUser(oid, (err, result) => {
-		let con1 = (result[0].sections[0].score.congruent)
-		let con2 = (result[0].sections[1].score.congruent)
-		let con3 = (result[0].sections[2].score.congruent)
-		let incon1 = (result[0].sections[0].score.incongruent)
-		let incon2 = (result[0].sections[1].score.incongruent)
-		let incon3 = (result[0].sections[2].score.incongruent)
-		let con = con1 + con2 + con3
-		let incon = incon1 + incon2 + incon3
+	const result = await db.selectHistoryAndUser(oid)
+	uid = result[0].user[0]._id
 
-		let correctStack = []
-		let testDayStack = []
-
-		for (i = 0; i < result[0].badge.length; i++) {
-			if (result[0].badge[i].type == 'correctStack') {
-				correctStack.push(result[0].badge[i])
-			}
-			if (result[0].badge[i].type == 'testDayStack') {
-				testDayStack.push(result[0].badge[i])
-			}
+	let hisResult = []
+	const userResult = await db.selectUserAndHistory(uid)
+	
+	for (i = 0; i < userResult[0].history.length; i++) {
+		var hid = userResult[0].history[i]._id
+		if (JSON.stringify(hid) !== JSON.stringify(oid)) {
+			hisResult.push(userResult[0].history[i])
 		}
+	}
 
-		res.render('history/history', {
-			result,
-			history: true,
-			con,
-			incon,
-			correctStack,
-			testDayStack
-		})
+	let con1 = (result[0].sections[0].score.congruent)
+	let con2 = (result[0].sections[1].score.congruent)
+	let con3 = (result[0].sections[2].score.congruent)
+	let incon1 = (result[0].sections[0].score.incongruent)
+	let incon2 = (result[0].sections[1].score.incongruent)
+	let incon3 = (result[0].sections[2].score.incongruent)
+	let con = con1 + con2 + con3
+	let incon = incon1 + incon2 + incon3
+
+	let correctStack = []
+	let testDayStack = []
+
+	for (i = 0; i < result[0].badge.length; i++) {
+		if (result[0].badge[i].type == 'correctStack') {
+			correctStack.push(result[0].badge[i])
+		}
+		if (result[0].badge[i].type == 'testDayStack') {
+			testDayStack.push(result[0].badge[i])
+		}
+	}
+
+	res.render('history/history', {
+		result,
+		history: true,
+		con,
+		incon,
+		correctStack,
+		testDayStack,
+		hisResult
 	})
 });
 
